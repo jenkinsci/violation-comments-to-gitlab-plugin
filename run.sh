7a83889a9c76ff723b7616a7aa4bac09342804e0
Tomas Bjerre <tomas.bjerre85@gmail.com>
2023-09-17 10:26:02 +0200

    fix: allow MSBuild in Program Files (x86) and support NU1701

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./mvnw versions:update-properties
-./mvnw -q hpi:run -Djava.util.logging.config.file=logging.properties -Djenkins.version=2.263.2 -Denforcer.skip=true
+./mvnw -q hpi:run -Djava.util.logging.config.file=logging.properties -Djenkins.version=2.401.1 -Denforcer.skip=true
